09c8a8c5850c508c5738ee7a3e8488a09176f3c3
Brandon Antillon <brandon.antillon.681@my.csun.edu>
2018-01-22 14:13:56 -0800

    changed names of files to personal_site

--- personal_blog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PersonalBlogConfig(AppConfig):
-    name = 'personal_blog'
--- personal_site_project/personal_site_project/wsgi.py
+++ personal_site_project/personal_site_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for personal_blog_project project.
+WSGI config for personal_site_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_blog_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_site_project.settings")
 
 application = get_wsgi_application()
